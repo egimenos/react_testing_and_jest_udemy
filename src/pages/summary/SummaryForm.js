6539ca7bcb68f1e034b16e8ab4e0850c0fe038b9
EgimenoS <ergisan@gmail.com>
2022-02-27 20:44:48 +0100

    tested and coded order confirmation flow happy path

--- src/pages/summary/SummaryForm.js
+++ src/pages/summary/SummaryForm.js
@@ -2,8 +2,16 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Popover, OverlayTrigger } from "react-bootstrap";
-export default function SummaryForm() {
+export default function SummaryForm({ setOrderPhase }) {
   const [tcChecked, setTcChecked] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase("completed");
+  }
 
   const popover = (
     <Popover id="popover-basic">
@@ -21,7 +29,7 @@ export default function SummaryForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="terms-and-conditions">
         <Form.Check
           type="checkbox"
